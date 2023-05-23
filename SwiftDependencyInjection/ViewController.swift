c4b89387b194aeb8f062dbf26f0fa280d529b7c2
serkanboyraci <serkanboyraci@gmail.com>
2023-05-23 13:02:13 +0200

    SWinject defined and added.

--- SwiftDependencyInjection/ViewController.swift
+++ SwiftDependencyInjection/ViewController.swift
@@ -6,8 +6,26 @@
 //
 
 import UIKit
+import Swinject
 
 class ViewController: UIViewController {
+    
+    //Swinject
+    let container : Container = {
+        let container = Container()
+        container.register(BackgroundProvidingClass.self) { resolver in
+            return BackgroundProvidingClass()
+        }
+        
+        container.register(AnotherVCViewController.self) { resolver in
+            let vc = AnotherVCViewController(providerProtocol: resolver.resolve(BackgroundProvidingClass.self))
+                return vc 
+        }
+            
+        
+        
+            return container
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
