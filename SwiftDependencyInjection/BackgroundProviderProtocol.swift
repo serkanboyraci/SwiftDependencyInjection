53c9ba726c3e88f6cba3233b18d7a65ec0ef76c5
serkanboyraci <serkanboyraci@gmail.com>
2023-05-23 11:56:49 +0200

    protocol added

---
+++ SwiftDependencyInjection/BackgroundProviderProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  BackgroundProviderProtocol.swift
+//  SwiftDependencyInjection
+//
+//  Created by Ali serkan Boyracı  on 23.05.2023.
+//
+
+import Foundation
+import UIKit
+
+protocol BackgroundProviderProtocol {
+    
+    var backgroundColor : UIColor {
+        get
+    }
+}
