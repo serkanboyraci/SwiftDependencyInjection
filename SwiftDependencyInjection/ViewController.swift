d1abd916362fbe15c31b0567496f6badde681709
serkanboyraci <serkanboyraci@gmail.com>
2023-05-23 11:54:02 +0200

    button added

--- SwiftDependencyInjection/ViewController.swift
+++ SwiftDependencyInjection/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 50))
+        button.setTitle("Open Other VC", for: .normal)
+        button.center = view.center
+        button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @objc private func buttonClicked() {
+        
     }
 
 
